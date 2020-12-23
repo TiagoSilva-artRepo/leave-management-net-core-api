882735b97d0d77f2d035ceafcf6ba4417cd535a1
Tiago Silva <tiago.silva@contisystems.pt>
2020-12-23 18:07:04 +0000

    Implement database and repository pattern

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,4 +1,8 @@
+using AutoMapper;
+using leave_management.Contracts;
 using leave_management.Data;
+using leave_management.Mappings;
+using leave_management.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +34,13 @@ namespace leave_management
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            //Add references for Repository and Contracts for Startup file
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddAutoMapper(typeof(Maps));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
